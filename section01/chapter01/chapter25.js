cdd30ce4466e9b00e629c292cbc8b4c03eab7ff5
xsaz0826 <xsaz0826@gmail.com>
2026-02-04 17:54:54 +0900

    [생성] 구조분해할당 배열을 변수에, 객체멤버 변수 할당

---
+++ section01/chapter01/chapter25.js
@@ -0,0 +1,33 @@
+//구조분해할당(배열을 분해하여 변수에 할당)
+let array = [1,2];
+let one = array[0];
+let two = array[1];
+let three = array[2];
+
+let [_one, _two, _three = 10, four = 4] = array;
+console.log(_one);
+console.log(_two);
+console.log(_three);
+console.log(four);
+
+//구조분해할당(객체를 분해하여 변수에 할당)
+let person = { 
+  name: "홍길동", 
+  age: 27, 
+  hobby: "테니스", 
+};
+
+let person3 = person;
+console.log(person3 === person);
+
+let {
+  age : myAge,
+  name : myName,
+  hobby : myHobby,
+  extra = "hello"
+} = person;
+
+let _myAge = person.age;
+let _myName = person.name;
+
+console.log(myAge, name, myHobby, extra);
